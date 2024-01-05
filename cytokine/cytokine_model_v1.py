3816d26d7de62b3cdaab1055f59ede911ecee2a2
Babak <babak.afshinpour@smiledirectclub.com>
2024-01-04 21:13:57 -0500

    feat/components added

---
+++ cytokine/cytokine_model_v1.py
@@ -0,0 +1,156 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy.integrate import odeint
+
+
+class Cytokine1(object):
+    def __init__(self, **kwargs):
+        """
+        mu_pro = 1/20, # rate of clearance
+        c0     = 10,   # rate of  background production
+        c1     = 10,   #
+        c2     = 50,   #
+        n1     = 2,
+        c3     = 10,   #(1/hr)
+        c4     = 40,   #(pg/ml)
+        n2     =  2,
+        c5     = 10,   # (1/hr)
+        I50    = 40,
+        mu_ant = 1/5,  #(1/hr)
+        c6     = 10,   #(pg/ml)
+        c7     = 5,    #1/hr
+        n3     = 2,
+        c8     = 30,   #(1/hr)
+        K50    = 40,   # (pg/ml)
+        mu_plasma = 1/8, # 1/hr
+        a0 = 0,
+        s0 = 20,# initial dosage (pg/ml)
+        p0 = 0  # initial dosage (pg/ml)
+        """
+
+        self.params = kwargs.copy()
+
+        p0 = self.params.pop("p0")
+        a0 = self.params.pop("a0")
+        s0 = self.params.pop("s0")
+
+        x0 = np.array([p0, a0, s0])
+
+        self.initial_values = x0
+        self.current_values = x0
+        self.last_evaluated_time = 0.0  # time zero when consent time
+        self.step_size = 1 / 60.0  # 1 minutes evaluation
+
+    def p_positive_feedback(self, p, c0=10, c1=10, c2=50, n1=2, **kwargs):
+        # pro-inflammator concetration (pg/ml)
+
+        # The background pro-inflammatory production rate
+        # c0    background production rate "(pg/ml/hr)"
+        # c1    maximum production rate "(pg/ml/hr)"
+        # c2    concentration where the half of maximum is secreted (pg/ml)
+
+        # n1  cytokine receptor expression
+
+        return c0 + c1 * (p**n1) / (c2**n1 + p**n1)
+
+    def p_inhibition(self, a, c3, c4, n2=2, **kwargs):
+        # c3   The regulation strength if anti-inflammatory cytokines acting on pro-inflammatory cytokines
+        # c4   The regulation strength if anti-inflammatory cytokines acting on pro-inflammatory cytokines
+        # n2   Cyto4kine receptor expression
+
+        return c3 * (c4**n2) / (c4**n2 + a**n2)
+
+    def p_steriod(self, s, c5, I50, **kwargs):
+        # c5   Efficiency of steroid on inhibiting pro-inflammatory
+        # I50  The concentration of steroid which binds to the half of the receptors
+        return c5 * (I50 / (I50 + s))
+
+    def pro_inflammatory_model(
+        self, p, a, s, mu_pro, c0, c1, c2, n1, c3, c4, n2, c5, I50, **kwargs
+    ):
+        dp = -mu_pro * p + self.p_positive_feedback(
+            p, c0, c1, c2, n1
+        ) * self.p_inhibition(a, c3, c4, n2) * self.p_steriod(s, c5, I50)
+        return dp
+
+    def a_upregulate(self, p, c6, c7, n3, **kwargs):
+        return c7 * ((p**n3) / (c6**n3 + p**n3))
+
+    def a_steriod(self, s, c8, K50, **kwargs):
+        return c8 * (K50 / (K50 + s))
+
+    def anti_inflammatory_model(self, p, a, s, mu_ant, c6, c7, n3, c8, K50, **kwargs):
+        da = -mu_ant * a + self.a_upregulate(p, c6, c7, n3) * self.a_steriod(s, c8, K50)
+        return da
+
+    def steriod(self, s, t, mu_plasma, **kwargs):
+        ds = -mu_plasma * s
+        return ds
+
+    def model(self, x, t):
+        (p, a, s) = x
+        dx = [
+            self.pro_inflammatory_model(p, a, s, **self.params),
+            self.anti_inflammatory_model(p, a, s, **self.params),
+            self.steriod(s, t, **self.params),
+        ]
+        return dx
+
+    def evaluate_model(self, t, initial_values=None):
+        """definition of function for LS fit
+        x gives evaluation points,
+        teta is an array of parameters to be varied for fit"""
+
+        if initial_values is None:
+            initial_values = self.initial_values
+
+        # create an alias to f which passes the optional params
+        model_with_params = lambda x, t: self.model(x, t)
+        # calculate ode solution, retuen values for each entry of "x"
+        r = odeint(model_with_params, initial_values, t)
+        # in this case, we only need one of the dependent variable values
+        return r
+
+    def step_to(self, time):
+        if self.last_evaluated_time < time:
+            t = np.arange(self.last_evaluated_time, time, self.step_size)
+            initial_values = self.current_values
+            out = self.evaluate_model(t, initial_values)
+            self.current_values = out[-1, :]
+            self.last_evaluated_time = time
+
+        return self.current_values
+
+    def reset(self):
+        self.current_values = self.initial_values
+        self.last_evaluated_time = 0.0
+
+
+if __name__ == "__main__":
+    model1 = Cytokine1(
+        mu_pro=1 / 20,  # rate of clearance
+        c0=10,  # rate of  background production
+        c1=10,  #
+        c2=50,  #
+        n1=2,
+        c3=10,  # (1/hr)
+        c4=40,  # (pg/ml)
+        n2=2,
+        c5=10,  # (1/hr)
+        I50=40,
+        mu_ant=1 / 5,  # (1/hr)
+        c6=10,  # (pg/ml)
+        c7=5,  # 1/hr
+        n3=2,
+        c8=30,  # (1/hr)
+        K50=40,  # (pg/ml)
+        mu_plasma=1 / 8,  # 1/hr
+        a0=0,
+        s0=40,  # initial dosage (pg/ml)
+        p0=0,  # initial dosage (pg/ml))
+    )
+    mm = []
+    for t in range(0, 30):
+        o = model1.step_to(t)
+        mm.append(o[2])
+    plt.plot(mm)
--- rl/environment.py
+++ rl/environment.py
@@ -1,202 +1,210 @@
 import gym
-from gym import spaces
-from cytokine_model_tiny import Cytokine1 
-from cytokine_model_normalized import CytokineNormalized 
 import numpy as np
-from dynamic_type_model import DT_GRU
-from drug_efficacy_model import DE_GRU
 import torch
+from gym import spaces
+
+from cytokine.cytokine_model_tiny import Cytokine1
+
 
 class CustomEnv(gym.Env):
     """Custom Environment that follows gym interface"""
-    metadata = {'render.modes': ['human']}
 
-    def __init__(self, steriod_max, observation_min,observation_max,step_size, eps_perb=0,behaviour = None, cytokine_model="normalized", **kwargs):
-        
+    metadata = {"render.modes": ["human"]}
+
+    def __init__(
+        self,
+        steriod_max,
+        observation_min,
+        observation_max,
+        step_size,
+        eps_perb=0,
+        behaviour=None,
+        cytokine_model="standard",
+        **kwargs,
+    ):
         super(CustomEnv, self).__init__()
         # Define action and observation space
         # They must be gym.spaces objects
-        
+
         self.params = kwargs.copy()
         self.cytokine_model = cytokine_model
-        self.behaviour  = behaviour
+        self.behaviour = behaviour
         self.eps_perb = eps_perb
-        if cytokine_model=="normalized":
-            self.cytokine = CytokineNormalized(**self.params )
+        if cytokine_model == "normalized":
+            raise RuntimeError("Not Implemented")
+            """
+            self.cytokine = CytokineNormalized(**self.params)
             if behaviour is not None:
                 self.cytokine.set_behaviour(behaviour)
             self.cytokine.perturb_parms(eps_perb)
-            
+            """
         else:
-            self.cytokine = Cytokine1(**self.params )
-         
-        self.step_size = 1 #(1 hour)
+            self.cytokine = Cytokine1(**self.params)
+
+        self.step_size = 1  # (1 hour)
         self.current_time = 0
         # Example when using discrete actions:
-        self.action_space = spaces.Box(low=np.array([0]),
-                                       high=np.array([steriod_max])) # 3 grams per day ?
-        
+        self.action_space = spaces.Box(
+            low=np.array([0]), high=np.array([steriod_max])
+        )  # 3 grams per day ?
+
         # Example for using image as input:
-        self.observation_space = spaces.Box(low=np.array(observation_min), 
-                                            high=np.array(observation_max)) # e.g. TNF-alpha
-        
+        self.observation_space = spaces.Box(
+            low=np.array(observation_min), high=np.array(observation_max)
+        )  # e.g. TNF-alpha
+
         self.observation_list = []
         self.action_list = []
-        
-        self.drug_model  = DE_GRU(n_features=2,n_hidden=20, n_output=1, n_layes=2)
+
+        self.drug_model = DE_GRU(n_features=2, n_hidden=20, n_output=1, n_layes=2)
         self.drug_model.init_hidden(batch_size=1)
         self.drug_model.load_model("./drug_efficacy_model.mdl")
-        
-        self.dynamic_type  = DT_GRU(n_features=1,n_hidden=20, n_output=5, n_layes=2)
-        self.dynamic_type.init_hidden(batch_size=1)
-        self.dynamic_type.load_model("./behaviorual_detection_model.mdl")     
 
-    
-    def call_dynamic_type(self,y):
+        self.dynamic_type = DT_GRU(n_features=1, n_hidden=20, n_output=5, n_layes=2)
+        self.dynamic_type.init_hidden(batch_size=1)
+        self.dynamic_type.load_model("./behaviorual_detection_model.mdl")
+
+    def call_dynamic_type(self, y):
         # y seqlen x 1
         self.dynamic_type.init_hidden(batch_size=1)
-        #print("dynamic",y[np.newaxis,...])
-        ttt = self.dynamic_type.forward(torch.FloatTensor(y[np.newaxis,...]).cuda(1))
-        out = ttt.detach().cpu().numpy().squeeze()  
-        out = out.reshape(-1,5)# seqlen x 5
-        return out
-    
-    def call_drug_model(self,y):
-        # y seqlen x 2
-        #print("drug",y[np.newaxis,...])
-        self.drug_model.init_hidden(batch_size=1)
-        ttt = self.drug_model.forward(torch.FloatTensor(y[np.newaxis,...]).cuda(1))
+        # print("dynamic",y[np.newaxis,...])
+        ttt = self.dynamic_type.forward(torch.FloatTensor(y[np.newaxis, ...]).cuda(1))
         out = ttt.detach().cpu().numpy().squeeze()
-        out = out.reshape(-1,1) # seqlen x 1
+        out = out.reshape(-1, 5)  # seqlen x 5
         return out
-        
-    
+
+    def call_drug_model(self, y):
+        # y seqlen x 2
+        # print("drug",y[np.newaxis,...])
+        self.drug_model.init_hidden(batch_size=1)
+        ttt = self.drug_model.forward(torch.FloatTensor(y[np.newaxis, ...]).cuda(1))
+        out = ttt.detach().cpu().numpy().squeeze()
+        out = out.reshape(-1, 1)  # seqlen x 1
+        return out
+
     # step every hour but problem solved in smaller time-scale
     def step(self, action_in):
         # Execute one time step within the environment
-        
-        #action = action[0]
-        #if (sum(self.action_list[-24:]) + action)>3:
-    
+
+        # action = action[0]
+        # if (sum(self.action_list[-24:]) + action)>3:
+
         #    action = np.array([3 - sum(self.action_list[-24:])])
         #    if action<0:
         #        action = np.array([0])
-        
+
         sact = sum(self.action_list[-24:])
-        '''
+        """
         if (sact+action[-1])>3:
             action0 = 3 - sact - action[-1]
             if action0<0:
                 action0 = 0
         
             action = [action0]
-        '''
-        
+        """
+
         action = [a for a in action_in]
-        if action[-1]<0:
+        if action[-1] < 0:
             action[-1] = 0
-        self.cytokine.current_values[-1] += action 
+        self.cytokine.current_values[-1] += action
         old_pro = self.cytokine.current_values[0]
-        observation,old_observation = self.cytokine.step_to(self.current_time+self.step_size)
+        observation, old_observation = self.cytokine.step_to(
+            self.current_time + self.step_size
+        )
         self.current_time += self.step_size
-        
-        
-        
-        
-        
-        all_obs_list = observation.tolist()#  + old_observation.tolist() + [sact]
+
+        all_obs_list = observation.tolist()  #  + old_observation.tolist() + [sact]
         observation = np.array(all_obs_list)
-        #print(observation)
+        # print(observation)
         self.observation_list.append(observation[0])
         self.action_list.append(action[-1])
-        
-        
-        
-        
+
         # reward  we can calculate the sum of last 24 hours and observation[1] # cytokine? to form a loss
         # done   48 hours should be done=True
-        
+
         s = np.mean(self.action_list[-24:])
-        reward_sigmoid = (1+s)
-        if observation[0]==0:
+        reward_sigmoid = 1 + s
+        if observation[0] == 0:
             reward = 1
         reward = -observation[0] - s + 1
-        
-        
+
         # -np.exp(observation[0])#1 - (observation[0]/(0.1 + old_observation[0]))   #np.exp(-observation[0])         #reward_sigmoid/(0.1+observation[0])
-        #(observation[0,1] - observation[0,0])/observation[0,1] #np.max(self.observation_list)
-     
-        
-        #print(reward)
-        
-         
-        
-        done =  self.current_time==48
-        info   = {}
+        # (observation[0,1] - observation[0,0])/observation[0,1] #np.max(self.observation_list)
+
+        # print(reward)
+
+        done = self.current_time == 48
+        info = {}
         info["action"] = action
-        
-        #import pdb
-        #pdb.set_trace()
-        #print(self.observation_list)
-        
-        dynamic_type,drug_eff = self.get_dynamic_drug_eff()
-        
-        #import pdb
-        #pdb.set_trace()
-        observation = np.array(observation.tolist()+[s]+dynamic_type+drug_eff)
-        
+
+        # import pdb
+        # pdb.set_trace()
+        # print(self.observation_list)
+
+        dynamic_type, drug_eff = self.get_dynamic_drug_eff()
+
+        # import pdb
+        # pdb.set_trace()
+        observation = np.array(observation.tolist() + [s] + dynamic_type + drug_eff)
+
         return observation, reward, done, info
-    
+
     def get_dynamic_drug_eff(self):
-        
-        all_observation_seq = np.array(self.observation_list)[:,np.newaxis]
+        all_observation_seq = np.array(self.observation_list)[:, np.newaxis]
         temp = all_observation_seq.copy()
-        normalizer = np.cumsum(temp,axis=0)/np.arange(1,temp.shape[0]+1,1)[:,np.newaxis]
+        normalizer = (
+            np.cumsum(temp, axis=0) / np.arange(1, temp.shape[0] + 1, 1)[:, np.newaxis]
+        )
         temp = temp / normalizer
         all_observation_seq = temp
-        
-        all_action_seq = np.array(self.action_list)[:,np.newaxis]
-        
+
+        all_action_seq = np.array(self.action_list)[:, np.newaxis]
+
         dynamic_type = self.call_dynamic_type(all_observation_seq)
         dynamic_type = dynamic_type[-1].tolist()
-        
-        
-        inp = np.concatenate((all_observation_seq,all_action_seq),axis=1)
+
+        inp = np.concatenate((all_observation_seq, all_action_seq), axis=1)
         drug_eff = self.call_drug_model(inp)
-        drug_eff = drug_eff[-1,:].tolist()
-        
-        return dynamic_type,drug_eff
-        
-    def reset(self,p0):
+        drug_eff = drug_eff[-1, :].tolist()
+
+        return dynamic_type, drug_eff
+
+    def reset(self, p0):
         # Reset the state of the environment to an initial state
         self.params["p0"] = p0
-        if self.cytokine_model=="normalized":
-            self.cytokine = CytokineNormalized(**self.params )
+        if self.cytokine_model == "normalized":
+            raise RuntimeError("Not implemented!")
+            """
+            self.cytokine = CytokineNormalized(**self.params)
             if self.behaviour is not None:
                 self.cytokine.set_behaviour(self.behaviour)
             self.cytokine.perturb_parms(self.eps_perb)
-        else:   
-            self.cytokine = Cytokine1(**self.params )
+            """
+        else:
+            self.cytokine = Cytokine1(**self.params)
         self.current_time = 0
         self.action_list = []
         self.observation_list = []
-        #print(self.cytokine.current_values[np.newaxis,:].shape)
-        #print(np.array([0])[np.newaxis,:].shape)
-        all_obs_list = self.cytokine.current_values.tolist()# + self.cytokine.current_values.tolist() + [0]
+        # print(self.cytokine.current_values[np.newaxis,:].shape)
+        # print(np.array([0])[np.newaxis,:].shape)
+        all_obs_list = (
+            self.cytokine.current_values.tolist()
+        )  # + self.cytokine.current_values.tolist() + [0]
         observation = np.array(all_obs_list)
-        
-        #np.concatenate([self.cytokine.current_values[np.newaxis,:],
+
+        # np.concatenate([self.cytokine.current_values[np.newaxis,:],
         #                             self.cytokine.current_values[np.newaxis,:],np.array([0])[np.newaxis,:]],axis=1)
-        
-        
+
         self.observation_list.append(observation[0])
         self.action_list.append(0)
-        #import pdb
-        #pdb.set_trace()
-        dynamic_type,drug_eff = self.get_dynamic_drug_eff()
-        observation = np.array(observation[:-1].tolist()+[0]+dynamic_type+drug_eff)
+        # import pdb
+        # pdb.set_trace()
+        dynamic_type, drug_eff = self.get_dynamic_drug_eff()
+        observation = np.array(
+            observation[:-1].tolist() + [0] + dynamic_type + drug_eff
+        )
         return observation
-    
-    def render(self, mode='human', close=False):
+
+    def render(self, mode="human", close=False):
         # Render the environment to the screen
         pass
